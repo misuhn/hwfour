1306c179e007f48984354800c6bf1b42007c5352
Miya Shin <mmshin@usc.edu>
2023-03-13 00:57:32 +0000

    donefinally

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,18 +1,34 @@
-#ifndef RECCHECK
-//if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
-#endif
-
 #include "equal-paths.h"
+#include <algorithm>
 using namespace std;
 
+int getHeight(Node* root, bool& valid)
+{
+    if (root == NULL) {
+        return 0;
+    }
 
-// You may add any prototypes of helper functions here
+    int leftHeight = getHeight(root->left, valid);
+    int rightHeight = getHeight(root->right, valid);
 
+    if ((!root->left) && (root->right)) {
+        return (1 + rightHeight);
+    }
+    else if ((root->left) && (!root->right)) {
+        return (1 + leftHeight);
+    }
+    else if ((root->left) && (root->right)) {
+        if (leftHeight != rightHeight) {
+            valid = false;
+        }
+    }
+
+    return (1 + (max(leftHeight, rightHeight)));
+}
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
-
+    bool valid = true;
+    getHeight(root, valid);
+    return valid;
 }
-
